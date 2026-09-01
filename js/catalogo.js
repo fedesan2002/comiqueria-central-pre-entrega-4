15c53d2d0f94ef1d354456a436126337491262ca
fedesan2002 <fedesan2002@gmail.com>
2026-08-31 22:23:38 -0300

    Update catalogo.js

--- js/catalogo.js
+++ js/catalogo.js
@@ -6,54 +6,99 @@ const catalogo = [
     "One Piece"
 ];
 
+
+
 function modificarCatalogo() {
+
+    
     catalogo.push("Dandadan");
     console.log("Se agregó Dandadan al final");
+
 
     catalogo.unshift("Kagurabachi");
     console.log("Se agregó Kagurabachi al principio");
 
+
     const eliminadoFinal = catalogo.pop();
-    console.log(`Se ha eliminado el elemento: ${eliminadoFinal}`);
+
+    console.log(
+        `Se ha eliminado el elemento: ${eliminadoFinal}`
+    );
+
 
     catalogo.unshift("Chainsaw Man");
 
-    const eliminadoInicio = catalogo.shift();
-    console.log(`Se eliminó del inicio: ${eliminadoInicio}`);
 
+    const eliminadoInicio = catalogo.shift();
+
+    console.log(
+        `Se eliminó del inicio: ${eliminadoInicio}`
+    );
+
+
+    // Modifica un elemento mediante su índice
     const indiceModificar = 2;
+
     const productoAnterior = catalogo[indiceModificar];
 
-    catalogo.splice(indiceModificar, 1, "Daredevil");
+
+    // Reemplaza el producto mediante splice
+    catalogo.splice(
+        indiceModificar,
+        1,
+        "Daredevil"
+    );
+
 
     console.log(
         `Se reemplazó "${productoAnterior}" por "${catalogo[indiceModificar]}"`
     );
 }
 
+
 function mostrarCatalogo(lista) {
+
     let mensaje = "CATÁLOGO DISPONIBLE\n\n";
 
+
     for (const comic of lista) {
+
         mensaje += `Producto: ${comic}\n`;
     }
 
+
     alert(mensaje);
+
     console.log(mensaje);
+
 
     return mensaje;
 }
 
+
+
 function buscarProducto(nombre) {
+
+    // Verifica si el producto existe
     if (catalogo.includes(nombre)) {
+
+        // Obtiene su posición
         const posicion = catalogo.indexOf(nombre);
 
-        alert(`${nombre} está disponible.\nÍndice: ${posicion}`);
+
+        alert(
+            `${nombre} está disponible.\nÍndice: ${posicion}`
+        );
+
 
         return true;
     }
 
-    alert(`${nombre} no se encuentra en el catálogo.`);
+
+    alert(
+        `${nombre} no se encuentra en el catálogo.`
+    );
+
 
     return false;
 }
